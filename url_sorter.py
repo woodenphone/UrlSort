ad4d2d4889c4a86a0481aebd9da42870cec7204c
James <J1459.j1459@gmail.com>
2013-05-18 12:38:34 +0800

    Cleaned up around username extractor code. Refactored export functions to make using both together fater. created export_from_file() to export both parsed and unparsed links together.

--- url_sorter.py
+++ url_sorter.py
@@ -55,15 +55,15 @@ def load_textfile(filepath):
         f.close()
 
 def save_text(filepath,data):
-    print 'save_text:filepath', filepath
+    #print 'save_text:filepath', filepath
     save_dir = os.path.dirname(filepath)
-    print 'save_text:save_dir', save_dir
+    #print 'save_text:save_dir', save_dir
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
     f = open(filepath,'w')
     f.write(data)
     f.close()
-    print 'save_text:Saved data to file', filepath
+    #print 'save_text:Saved data to file', filepath
 
 def extract_domain(url):
     #return the domain from given url
@@ -91,7 +91,7 @@ def sanitize_filename(filename):
 def build_link_dict(unsorted_data):
     #turn a string with urls in it into a dict using format {'DomainName.com':['url1','url2']}
     url_list = extractlinks(unsorted_data)
-    print 'url_list', url_list
+    #print 'url_list', url_list
     sorting_dict = {}# {'DomainOne.com':['url1','url2']}
     for url in url_list:
         # print 'url',  url
@@ -105,15 +105,18 @@ def build_link_dict(unsorted_data):
 def export_urls_from_file(input_file_path='paste_here.txt'):
     #read the specified text file and output a list of links for each domain
     unsorted_data = load_textfile(input_file_path)
-    print 'unsorted_data', unsorted_data
+    #print 'unsorted_data', unsorted_data
     link_dict = build_link_dict(unsorted_data)
+    export_urls_from_dict(link_dict)
+
+def export_urls_from_dict(link_dict):
     for domain_key in link_dict.keys():
-        print 'domain_key', domain_key
+        #print 'domain_key', domain_key
         output_filename = sanitize_filename(domain_key) + '.txt'
         output_data = ''
         for output_url in link_dict[domain_key]:
             output_data += (output_url + '\n')
-        print 'output_data', output_data
+        #print 'output_data', output_data
         output_path = 'output/' + output_filename
         save_text(output_path,output_data)
 
@@ -129,7 +132,6 @@ def deviantart_convert(url):
         username = username_search.group(1)
         return username
 
-
 def furaffinity_convert(url):
     # Turn a furaffinity URL into a furaffinity username.
     # Valid URL examples:
@@ -143,7 +145,6 @@ def furaffinity_convert(url):
             username = username[0:-1]
         return username
         #print 'furaffinity_convert: username:',username
-
 
 def inkbunny_convert(url):
     # Turn an InkBunny URL into an InkBunny username.
@@ -164,7 +165,6 @@ def inkbunny_convert(url):
     if username_search:
         username = username_search.group(1)
         return username
-
 
 def tumblr_convert(url):
     # Turn a Tumblr URL into a Tumblr username.
@@ -205,15 +205,16 @@ def aryion_convert(url):# TODO
     if username_search:
         username = username_search.group(1)
         return username
-
-
 # End converter functions
 
 def export_usernames_from_file(input_file_path='paste_here.txt'):
     #read the specified text file and output a list of usernames for each recognized domain
     unsorted_data = load_textfile(input_file_path)
-    print 'unsorted_data', unsorted_data
+    #print 'unsorted_data', unsorted_data
     link_dict = build_link_dict(unsorted_data)
+    export_usernames_from_dict(link_dict)
+
+def export_usernames_from_dict(link_dict):
     for domain_key in link_dict.keys():
         print 'domain_key', domain_key
         output_filename = sanitize_filename(domain_key) + '.txt'
@@ -251,9 +252,15 @@ def export_usernames_from_file(input_file_path='paste_here.txt'):
         output_path = 'parsed_output/' + output_filename
         save_text(output_path, output_string)
 
+def export_from_file(input_file_path='paste_here.txt'):
+    unsorted_data = load_textfile(input_file_path)
+    link_dict = build_link_dict(unsorted_data)
+    export_usernames_from_dict(link_dict)
+    export_urls_from_dict(link_dict)
+
+
 def main():
-    #export_urls_from_file()
-    export_usernames_from_file()
+    export_from_file()
     print 'Job done.'
     raw_input('Press Return to exit.')
 
